c3170884753455e5cae6e6a744a82b25d98655e2
26jkim <33298038+26jkim@users.noreply.github.com>
2018-03-23 14:41:25 -0600

    updates and it's broken

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-document.getElementById("app").innerHTML = `
-<h1>Hello Parcel!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
-`;
+import Phaser from "phaser";
+
+let game = new Phaser.Game(960, 600, Phaser.AUTO, "game");
